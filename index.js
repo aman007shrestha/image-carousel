b7f9beaf9bf50e13775c360fb053bfff0e7f8ee3
aman007shrestha <aman13xrestha@gmail.com>
2022-06-14 08:39:40 +0545

    change index.js

--- index.js
+++ index.js
@@ -6,6 +6,4 @@ app.use(express.static('public'));
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname + '/public/index.html'));
 });
-app.listen(PORT, () => {
-  console.log('Server running at 3000');
-});
+app.listen(PORT);
